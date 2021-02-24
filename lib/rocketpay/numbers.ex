bc78bb1846092efad03b51fded30fcb2d42f9d0f
David Guimarães <davidguimaraes@digix.com.br>
2021-02-23 23:37:25 -0400

    06 - Melhorando o pattern matching e introduzindo o pipe operator.

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -1,10 +1,11 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read!("#{filename}.csv")
-    handle_file(file)
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
   end
 
-  # defp handle_file({:ok, file}), do: file
-  defp handle_file(file), do: file
+  defp handle_file({:ok, file}), do: file
+  # defp handle_file(file), do: file
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 end
