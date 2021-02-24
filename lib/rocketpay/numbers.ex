11043f910e08f17c360414bd2e0b8d3a6f8605ec
David Guimarães <davidguimaraes@digix.com.br>
2021-02-23 23:51:30 -0400

    07 - Implementando a regra de somar os números do csv e conhecendo os módulos String e Enum.

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -2,10 +2,18 @@ defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
     "#{filename}.csv"
     |> File.read()
-    |> handle_file()
+    |> handle_file_content()
   end
 
-  defp handle_file({:ok, file}), do: file
-  # defp handle_file(file), do: file
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+  defp handle_file_content({:ok, file_content}) do
+    result =
+      file_content
+      |> String.split(",")
+      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file_content({:error, _reason}), do: {:error, "Invalid file!"}
 end
